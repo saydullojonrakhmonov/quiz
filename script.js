aeeace6ba9ac35dc5c768d9b67176e8a201d57ea
Saydullo <saydullo779@gmail.com>
2024-12-22 18:21:31 +0500

    I selected all elements in DOM

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Selecting modal
 const settingsBtn = document.querySelector(".settings-btn");
 const modal = document.getElementById("modal");
 const applyBtn = document.querySelector(".apply-btn");
@@ -5,8 +6,50 @@ const quizType = document.getElementById("quiz-types");
 const quizAmount = document.getElementById("quiz-amount");
 const quizCategory = document.getElementById("quiz-category");
 
-settingsBtn.addEventListener('click', () =>{
-  modal.classList.toggle('hide')
-})
+// Selecting questions container
+const startBtn = document.getElementById("start-btn");
+const questionsBox = document.querySelector(".question-box");
+const questionTitle = document.querySelector(".question-title");
+const exitBtn = document.getElementById("exit");
+const starQuiztBtn = document.getElementById("start");
+const quizBox = document.getElementById("quiz-box");
+const timeLeftText = document.getElementById("time-left-text");
+const secondTimer = document.getElementById("timer-second");
+const timeLine = document.getElementById("time-line");
+const optionsList = document.getElementById("options-list");
+const resultBox = document.getElementById("result-box");
+const totalQuestion = document.getElementById('total-questions')
+const nextBtn = document.getElementById('next-btn')
+const restartBtn = document.getElementById("restart-btn");
+const quitBtn = document.getElementById("quit-btn");
+
+settingsBtn.addEventListener("click", () => {
+  modal.classList.toggle("hide");
+});
 
 let changeLevels;
+
+applyBtn.addEventListener("click", () => {
+  clearInterval(changeLevels);
+
+  changeLevels = setTimeout(() => {
+    const selectedDifficulty = quizType.value;
+    const selectedAmount = quizAmount.value;
+    const selectedCategory = quizCategory.value;
+
+    if (!selectedDifficulty || !selectedAmount || !selectedCategory) {
+      alert(
+        `Plese select difficulty level level, questions amount and category`
+      );
+      return;
+    }
+
+    axios
+      .get(
+        `https://opentdb.com/api.php?amount=${selectedAmount}&category=${selectCategory}&difficulty=${selectedDifficulty}&type=multiple`
+      )
+      .then((res) => {
+        questions = res.data.results.map((question, index) => {});
+      });
+  });
+});
